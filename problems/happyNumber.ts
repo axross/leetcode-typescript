73007881398ab7e165e14cf6aa038fb86b4e1bfa
Kohei Asai <me@axross.io>
2019-08-09 11:23:28 -0700

    202. Happy Number

---
+++ problems/happyNumber.ts
@@ -0,0 +1,24 @@
+// 202. Happy Number
+// https://leetcode.com/problems/happy-number/
+function isHappy(n: number): boolean {
+  if (n === 1) return true;
+
+  // any non-happy number is going to be 4 at certain point
+  if (n === 4) return false;
+
+  return isHappy(
+    `${n}`
+      .split("")
+      .reduce((total, digit) => total + Math.pow(parseInt(digit), 2), 0)
+  );
+}
+
+describe("202. Happy Number", () => {
+  test("#1", () => {
+    expect(isHappy(19)).toBe(true);
+  });
+
+  test("#2", () => {
+    expect(isHappy(18)).toBe(false);
+  });
+});
