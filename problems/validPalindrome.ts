bbc404dd74f55a53fdd54043284b62c2a88076ac
Kohei Asai <me@axross.io>
2019-08-09 13:40:22 -0700

    125. Valid Palindrome

---
+++ problems/validPalindrome.ts
@@ -0,0 +1,52 @@
+// 125. Valid Palindrome
+// https://leetcode.com/problems/valid-palindrome/
+function isPalindrome(s: string): boolean {
+  const sanitized = [];
+
+  for (let i = 0; i < s.length; ++i) {
+    const charCode = s.charCodeAt(i);
+
+    if (
+      // 0-9
+      (charCode >= 48 && charCode <= 57) ||
+      // a-z
+      (charCode >= 97 && charCode <= 122)
+    ) {
+      sanitized.push(s[i]);
+    }
+
+    // A-Z
+    if (charCode >= 65 && charCode <= 90) {
+      // A-Z -> a-z
+      sanitized.push(String.fromCharCode(charCode + 32));
+    }
+
+    // do nothing to ignore other characters
+  }
+
+  for (let i = 0; i < sanitized.length / 2; ++i) {
+    if (sanitized[i] !== sanitized[sanitized.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+describe("125. Valid Palindrome", () => {
+  test("#1", () => {
+    expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
+  });
+
+  test("#2", () => {
+    expect(isPalindrome("race a car")).toBe(false);
+  });
+
+  test("#3", () => {
+    expect(isPalindrome("")).toBe(true);
+  });
+
+  test.only("#4", () => {
+    expect(isPalindrome("0P")).toBe(false);
+  });
+});
