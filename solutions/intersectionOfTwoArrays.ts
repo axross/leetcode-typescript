5105400a42f3ccfdab2aa83fd89f7df07b3b4dd8
Kohei Asai <me@axross.io>
2019-08-11 14:32:57 -0700

    349. Intersection of Two Arrays (#49)

---
+++ solutions/intersectionOfTwoArrays.ts
@@ -0,0 +1,21 @@
+// 349. Intersection of Two Arrays
+// https://leetcode.com/problems/intersection-of-two-arrays/
+function intersection(nums1: number[], nums2: number[]): number[] {
+  const set = new Set();
+  const result: number[] = [];
+
+  for (const num of nums1) {
+    set.add(num);
+  }
+
+  for (const num of nums2) {
+    if (set.has(num)) {
+      result.push(num);
+      set.delete(num);
+    }
+  }
+
+  return result;
+}
+
+export default intersection;
