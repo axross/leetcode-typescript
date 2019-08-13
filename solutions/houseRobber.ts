857fb8b935753cb467be29921306ecc645b4357d
Kohei Asai <me@axross.io>
2019-08-12 17:20:30 -0700

    Update comments of dynamic programming solutions (#57)

--- solutions/houseRobber.ts
+++ solutions/houseRobber.ts
@@ -1,20 +1,8 @@
 // 198. House Robber
 // https://leetcode.com/problems/house-robber/
 function rob(nums: number[]): number {
-  // this problem can be solved with dynamic programming
-  //
-  // for example when the nums is [2, 4, 9, 3, 1, 5], recurrence relation is:
-  //
-  // dp[-1] = 0
-  // dp[0] = 0
-  // dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
-  //
-  // dp table is:
-  //
-  //  -1   0   1   2   3   4   5   6
-  //   0   0   2   4  11  11  12  16
-  //
-  // so, initialize dp table that dp[-1] = 0 and dp[0] = 0
+  // this is dynamic programming.
+  // see https://github.com/axross/leetcode-typescript/pull/55
   const dp = new Map([[-1, 0], [0, 0]]);
 
   for (let i = 1; i <= nums.length; ++i) {
