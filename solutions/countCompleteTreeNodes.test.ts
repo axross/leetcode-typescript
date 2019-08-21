3017d363256bf5312b9a7d9ac77096345472b3c0
Kohei Asai <me@axross.io>
2019-08-21 13:20:17 -0700

    Refactor TreeNode utility

--- solutions/countCompleteTreeNodes.test.ts
+++ solutions/countCompleteTreeNodes.test.ts
@@ -1,9 +1,9 @@
-import { createTreeNode } from "../utilities/TreeNode";
+import { createBinaryTreeNode } from "../utilities/TreeNode";
 import countNodes from "./countCompleteTreeNodes";
 
 describe("222. Count Complete Tree Nodes", () => {
   test("#1", () => {
-    expect(countNodes(createTreeNode([1, 2, 3, 4, 5, 6]))).toBe(6);
+    expect(countNodes(createBinaryTreeNode([1, 2, 3, 4, 5, 6]))).toBe(6);
   });
 
   test("#2", () => {
@@ -11,10 +11,10 @@ describe("222. Count Complete Tree Nodes", () => {
   });
 
   test("#3", () => {
-    expect(countNodes(createTreeNode([1, 2, 3, 4, 5, 6, 7, 8]))).toBe(8);
+    expect(countNodes(createBinaryTreeNode([1, 2, 3, 4, 5, 6, 7, 8]))).toBe(8);
   });
 
   test("#4", () => {
-    expect(countNodes(createTreeNode([1, 2, 3, 4, 5, 6, 7]))).toBe(7);
+    expect(countNodes(createBinaryTreeNode([1, 2, 3, 4, 5, 6, 7]))).toBe(7);
   });
 });
--- utilities/TreeNode.ts
+++ utilities/TreeNode.ts
@@ -4,20 +4,47 @@ export interface TreeNode<T> {
   right: TreeNode<T> | null;
 }
 
-export function createTreeNode<T>(array: T[], selfIndex = 0): TreeNode<T> {
-  if (array.length === 0) {
-    throw TypeError("cannot accept a no-element array");
+export function createBinaryTreeNode<T>(
+  array: (T | null)[]
+): TreeNode<T> | null {
+  if (array.length === 0) return null;
+
+  let skipped = 0;
+  const root = { val: array[0]!, left: null, right: null };
+  const queue: [number, TreeNode<T>, BinaryTreeSide][] = [
+    [1, root, BinaryTreeSide.left],
+    [2, root, BinaryTreeSide.right]
+  ];
+
+  while (queue.length >= 1) {
+    const [index, parent, side] = queue.shift()!;
+
+    if (index >= array.length) break;
+
+    if (array[index] === null) {
+      skipped += 1;
+
+      continue;
+    }
+
+    const node = { val: array[index]!, left: null, right: null };
+
+    if (side === BinaryTreeSide.left) {
+      parent.left = node;
+    } else {
+      parent.right = node;
+    }
+
+    queue.push(
+      [(index - skipped) * 2 + 1, node, BinaryTreeSide.left],
+      [(index - skipped) * 2 + 2, node, BinaryTreeSide.right]
+    );
   }
 
-  return {
-    val: array[selfIndex],
-    left:
-      array[selfIndex * 2 + 1] != null
-        ? createTreeNode(array, selfIndex * 2 + 1)
-        : null,
-    right:
-      array[selfIndex * 2 + 2] != null
-        ? createTreeNode(array, selfIndex * 2 + 2)
-        : null
-  };
+  return root;
+}
+
+enum BinaryTreeSide {
+  left,
+  right
 }
