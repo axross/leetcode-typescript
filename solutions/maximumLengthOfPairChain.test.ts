92b78e126b491c9dfcbc6be88554e07e6b1dd458
Kohei Asai <me@axross.io>
2019-08-25 22:23:48 -0700

    646. Maximum Length of Pair Chain

---
+++ solutions/maximumLengthOfPairChain.test.ts
@@ -0,0 +1,19 @@
+import findLongestChain from "./maximumLengthOfPairChain";
+
+describe("646. Maximum Length of Pair Chain", () => {
+  test("#1", () => {
+    expect(findLongestChain([[1, 2], [2, 3], [3, 4]])).toBe(2);
+  });
+
+  test("#2", () => {
+    expect(findLongestChain([[3, 4], [2, 3], [1, 2]])).toBe(2);
+  });
+
+  test("#3", () => {
+    expect(findLongestChain([[1, 2]])).toBe(1);
+  });
+
+  test("#4", () => {
+    expect(findLongestChain([])).toBe(0);
+  });
+});
