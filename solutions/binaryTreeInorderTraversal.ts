54b0cf4682679f43a6b32e02828913afcaaa42a4
Kohei Asai <me@axross.io>
2019-09-25 18:33:21 -0700

    94. Binary Tree Inorder Traversal (#96)

---
+++ solutions/binaryTreeInorderTraversal.ts
@@ -0,0 +1,21 @@
+import { TreeNode } from "../utilities/TreeNode";
+
+// 94. Binary Tree Inorder Traversal
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+export default function inorderTraversal<T>(root: TreeNode<T> | null): T[] {
+  const history: T[] = [];
+
+  function traverse(node: TreeNode<T> | null): void {
+    if (node === null) return;
+
+    traverse(node.left);
+
+    history.push(node.val);
+
+    traverse(node.right);
+  }
+
+  traverse(root);
+
+  return history;
+}
