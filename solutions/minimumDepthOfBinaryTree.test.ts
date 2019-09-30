be8848fb0847315c94d5a564deb9fb3dc51754d2
Kohei Asai <me@axross.io>
2019-09-29 20:53:48 -0700

    111. Minimum Depth of Binary Tree (#113)

---
+++ solutions/minimumDepthOfBinaryTree.test.ts
@@ -0,0 +1,18 @@
+import { createBinaryTreeNode } from "../utilities/TreeNode";
+import minDepth from "./minimumDepthOfBinaryTree";
+
+describe("111. Minimum Depth of Binary Tree", () => {
+  const TEST_CASES = new Map([
+    [[3, 9, 20, null, null, 15, 7], 2],
+    [[1, 2], 2],
+    [[1, 2, null, null, null, 2], 3],
+    [[1], 1],
+    [[], 0]
+  ]);
+
+  for (const [values, expected] of TEST_CASES) {
+    it(`returns ${expected} when called with [${values}]`, () => {
+      expect(minDepth(createBinaryTreeNode(values))).toBe(expected);
+    });
+  }
+});
