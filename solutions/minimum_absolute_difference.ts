7997130f4b8e54ba1a31fbd763a3824ae5e8a1c6
Kohei Asai <me@axross.io>
2019-10-09 09:24:39 -0700

    1200. Minimum Absolute Difference

---
+++ solutions/minimum_absolute_difference.ts
@@ -0,0 +1,24 @@
+// 1200. Minimum Absolute Difference
+// https://leetcode.com/problems/minimum-absolute-difference/
+export default function minimumAbsDifference(arr: number[]): [number, number][] {
+  arr.sort((a, b) => a - b);
+
+  let pairs: [number, number][] = [];
+  let minimumDiff = Infinity;
+
+  for (let i = 0; i < arr.length - 1; ++i) {
+    const diff = arr[i + 1] - arr[i];
+
+    if (diff > minimumDiff) continue;
+
+    if (diff === minimumDiff) {
+      pairs.push([arr[i], arr[i+1]]);
+    } else if (diff < minimumDiff) {
+      pairs = [[arr[i], arr[i+1]]];
+    }
+
+    minimumDiff = diff;
+  }
+
+  return pairs;
+}
